2220f6134c6413c2b3cd6f01c462c663668224bb
Paul Kline <pauliankline@gmail.com>
2014-12-05 20:30:07 -0600

    pushing from apt.

--- ISpi.hs
+++ ISpi.hs
@@ -2,11 +2,21 @@
 module ISpi where
 
 
+import Control.Monad
+--Pi
 data Pi = Name String
 	| Pair Pi Pi
 	| Zero
 	| Succ Pi
-        | Var String deriving (Show, Eq)
+        | Var String deriving (Show, Eq)        
+--Pi types
+data PiType = TName   
+            | TPair PiType PiType
+            | TSucc
+            | TVar
+            | TZero deriving (Show, Eq)
+
+--PiProcess       
 data PiProcess = Output Pi Pi PiProcess
                | Input Pi (Pi) PiProcess --Input Pi (Var String) PiProcess
                | Composition PiProcess PiProcess
@@ -16,34 +26,118 @@ data PiProcess = Output Pi Pi PiProcess
                | Nil
                | Let (Pi,Pi) Pi PiProcess
                | Case Pi Pi PiProcess Pi PiProcess deriving (Show, Eq)
-               
+example1PiProcess = Output (Name "channel") (Name "message") Nil
+example2PiProcess = Input (Name "channel") (Name "message") Nil
+example3CompositionPiProcess = Composition example1PiProcess example2PiProcess
+--PiProcess types
+data PiProcessType = TOutput PiProcessType
+                   | TInput PiProcessType
+                   | TComposition PiProcessType PiProcessType
+                   | TRestriction PiProcessType
+                   | TReplication PiProcessType
+                   | TMatch PiProcessType
+                   | TNil
+                   | TLet PiProcessType
+                   | TCase deriving (Eq, Show)
+        
+data Protocol = Proto PiProcess
+              | Protocol `Then` Protocol
         
         
-        
-        
-        
---Pi type checker
-data PiType = TPiNumber 
-       | TPiNonNumber 
-       | TPair PiType PiType deriving (Show, Eq)
 
 
 
 typeCheckPi :: Pi -> Either String PiType
-typeCheckPi (Name str) = Right TPiNonNumber
+typeCheckPi (Name str) = Right TName
 typeCheckPi (Pair a b) = case typeCheckPi a of
                                 Left str -> Left ("First of Pair type fail: " ++ str) 
                                 Right fstT -> case typeCheckPi b of
                                                 Left str -> Left ("Second member of Pair type fail: " ++ str)
                                                 Right sndT ->  Right (TPair fstT sndT)                                
-typeCheckPi Zero = Right TPiNumber
+typeCheckPi Zero = Right TZero
 typeCheckPi x@(Succ t) = case typeCheckPi t of
                               Left str -> Left (str) --no extra error text here to prevent recursive error message. 
-                              Right TPiNumber -> Right TPiNumber
+                              Right TZero -> Right TSucc
+                              Right TSucc -> Right TSucc
+                              Right TVar  -> Right TSucc -- variables are okay, but may cause runtime error
                               Right _ -> Left ("Succ of non-numberic type: " ++ (show x))
-typeCheckPi (Var _) = Right TPiNonNumber                              
+typeCheckPi (Var _) = Right TVar                              
 typeCheckPi x = Left ("Type failure: " ++ (show x))
 
+
 testPiGood = Pair (Succ (Succ (Succ Zero))) (Pair (Name "namehere") (Pair Zero (Var "varhere")))
-testPi1Bad = Pair (Succ (Succ (Succ (Var "varhere1")))) (Pair (Name "namehere") (Pair Zero (Var "varhere")))
-testPi2Bad = Pair (Succ (Succ (Succ (Zero)))) (Pair (Name "namehere") (Pair Zero (Var "varhere")))
+testPi1Good = Pair (Succ (Succ (Succ (Var "varhere1")))) (Pair (Name "namehere") (Pair Zero (Var "varhere")))
+testPi2Bad = Pair (Succ (Succ (Succ (Name "name")))) (Pair (Name "namehere") (Pair Zero (Name "varhere")))
+
+typeCheckPiProcess :: PiProcess -> Either String PiProcessType
+typeCheckPiProcess (Input pi1 pi2 piProPrime) = case typeCheckPi pi1 of
+      Left err   -> Left ("term 'Input' type fail on first pi argument: " ++ err)
+      Right tpi1 -> case typeCheckPi pi2 of
+         Left err   -> Left ("term 'Input' type fail on second pi argument: " ++ err)
+         Right tpi2 -> case typeCheckPiProcess piProPrime of
+            Left err     -> Left ("terpm 'Input' type fail on process subterm: " ++ err)
+            Right primeT -> Right (TInput primeT)
+typeCheckPiProcess (Output pi1 pi2 piProPrime) = case typeCheckPi pi1 of
+      Left err   -> Left ("term 'Output' type fail on first pi argument: " ++ err)
+      Right tpi1 -> case typeCheckPi pi2 of
+         Left err   -> Left ("term 'Output' type fail on second pi argument: " ++ err)
+         Right tpi2 -> case typeCheckPiProcess piProPrime of
+            Left err     -> Left ("terpm 'Output' type fail on process subterm: " ++ err)
+            Right primeT -> Right (TOutput primeT)
+typeCheckPiProcess (Composition proc1 proc2) = case typeCheckPiProcess proc1 of
+      Left err     -> Left ("Type error in term 'Composition', first process: " ++ err)
+      Right proc1T -> case typeCheckPiProcess proc1 of
+         Left err     -> Left ("Type error in term 'Composition', second process: " ++ err)
+         Right proc2T -> Right (TComposition proc1T proc2T)
+typeCheckPiProcess (Restriction pi1 proc) = case typeCheckPi pi1 of
+      Left err   -> Left ("Type error in term 'Restriction' Pi term: " ++ err)
+      Right pi1T -> case typeCheckPiProcess proc of
+         Left err   -> Left ("Type error in term 'Restriction' Process term: " ++ err)
+         Right procT -> Right (TRestriction procT)
+typeCheckPiProcess (Replication proc) = case typeCheckPiProcess proc of
+      Left err -> Left ("Type error in term 'Replication': " ++ err)
+      Right procT -> Right (TReplication procT)
+typeCheckPiProcess (Match pi1 pi2 piProPrime) = case typeCheckPi pi1 of
+      Left err   -> Left ("term 'Match' type fail on first pi argument: " ++ err)
+      Right tpi1 -> case typeCheckPi pi2 of
+         Left err   -> Left ("term 'Match' type fail on second pi argument: " ++ err)
+         Right tpi2 -> case typeCheckPiProcess piProPrime of
+            Left err     -> Left ("term 'Match' type fail on process subterm: " ++ err)
+            Right primeT -> Right (TMatch primeT) 
+typeCheckPiProcess Nil = Right TNil
+typeCheckPiProcess (Let (pi1,pi2) pairPossibly proc) = case typeCheckPi pairPossibly of
+      (Right pairPossiblyT) -> if acceptablePi (TPair TZero TZero) pairPossiblyT then
+                                 case (typeCheckPi pi1, typeCheckPi pi2) of
+                                    (Right TVar, Right TVar) -> case typeCheckPiProcess proc of
+                                                   (Right procT) -> Right (TLet procT)
+                                                   (Left err)    -> Left ("Type error in Let subprocess: " ++ err)
+                                    (tup)                    -> Left ("Type error. Expected 2 variables in let binding. Found: " ++ join (map (either show show) (listify2 tup)) )           
+                                else Left ("Type error. Expected type Tpair in Let binding. Actual type: " ++ (show pairPossiblyT))    
+      (Left err)            -> Left ("Type error in pair value of Let: " ++ err)                                
+typeCheckPiProcess (Case pi0 pi1 piproc1 pi2 piproc2) = case (typeCheckPi pi0, typeCheckPi pi1, typeCheckPi pi2) of
+      (Right pi0T, Right pi1T, Right pi2T) -> case typeCheckPiProcess piproc1 of
+                                                (Right piproc1T) -> case typeCheckPiProcess piproc2 of
+                                                                     (Right piproc2T) -> Right TCase
+                                                                     (Left err)       -> Left ("Type Error in second process of Case: " ++ err)
+                                                (Left err)       -> Left ("Type Error in first process of Case: " ++ err)
+      triplet                              -> Left ("Error in a pi term of Case statement. Actual types: " ++ join ((map (either show show) (listify3 triplet))))
+      
+acceptablePi :: PiType -> PiType -> Bool
+acceptablePi (TPair _ _) type2 = case type2 of
+                                  (TPair _ _) -> True
+                                  (TVar)      -> True
+                                  _           -> False
+                                 
+                     
+listify2 :: (a,a) -> [a]
+listify2 (x,y) = [x,y]
+
+listify3 :: (a,a,a) -> [a]
+listify3 (x,y,z) = [x,y,z]
+
+                                                                                                                                                                                      
+                                                                                           
+                                                                                           
+                                                                                           
+                                                                                           
+                                                                                           
